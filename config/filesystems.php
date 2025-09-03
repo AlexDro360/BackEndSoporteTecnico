737f601e018443d66d00c170261661f438408020
CarlosE0204 <21160591@itoaxaca.edu.mx>
2025-09-03 10:54:20 -0600

    Coreccion del sotorage

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,14 +58,14 @@ return [
         
         'pdfsRespuestas' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/pdfs/respuestas'),
+            'root' => storage_path('app/pdfs/respuestas'),
             'url' => env('APP_URL').'/storage/respuestas',
             'throw' => false,
         ],
 
         'pdfsSolicitudes' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/pdfs//solicitudes'),
+            'root' => storage_path('app/pdfs/solicitudes'),
             'url' => env('APP_URL').'/storage/solicitudues',
             'throw' => false,
         ]
