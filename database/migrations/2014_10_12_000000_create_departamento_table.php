987f51ca300b1e9b77eb0410e2bc2a2803a52f27
AlexDro360 <21160666@itoaxaca.edu.mx>
2026-02-16 13:03:57 -0600

    Dep

--- database/migrations/2014_10_12_000000_create_departamento_table.php
+++ database/migrations/2014_10_12_000000_create_departamento_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('departamentos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre',100);
-            $table->string('abreviatura', 5);
+            $table->string('abreviatura', 10);
             $table->integer('folio');
             $table->timestamps();
         });
